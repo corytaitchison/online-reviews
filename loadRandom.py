de52462c70c1a18bc0ee14f250f2aa600c7818ca
Cory Aitchison <corytaitchison@gmail.com>
2019-08-29 10:13:08 +1000

    Create random sampling function

---
+++ loadRandom.py
@@ -0,0 +1,34 @@
+import numpy as np
+import random
+from loadData import loadJSON
+import pandas as pd
+import sys
+
+
+def loadRandom(location, k=10, targetNum=1e3, seed=12345):
+    # Location = location of file to load in, k = number of partitions
+    random.seed(seed)
+    # Relative number of observations per chunk
+    weights = np.array(random.sample(range(1, 1000), k))
+    chunkSize = int(6e6/k)  # TODO: Replace with actual size of the data
+    # Array of number of observations per chunk
+    partitions = (weights * chunkSize / sum(weights)).astype(int)
+
+    returnPanda = pd.DataFrame()
+
+    for i in range(k):
+        chunk = ((i)*chunkSize, (i+1)*chunkSize)
+
+        # Load file based on file type
+        if "csv" in location:
+            # data = loadCSV(location, chunk[1], chunk[0])
+            pass
+        elif "json" in location:
+            data = loadJSON(location, chunk[1], chunk[0])
+        else:
+            sys.exit("Unknown file - please specify correct csv or json file")
+        indices = random.sample(range(chunkSize), partitions[i])
+        data = data.iloc[indices, ]
+        returnPanda = returnPanda.append(data)
+
+    return returnPanda
