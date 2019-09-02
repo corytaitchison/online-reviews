cd97a8253c0b031d5df8cce814432dfc3c68a49e
Cory Aitchison <corytaitchison@gmail.com>
2019-09-03 07:12:11 +1000

    Fix random loading

--- loadRandom.py
+++ loadRandom.py
@@ -1,6 +1,7 @@
 import random
 import pandas as pd
 import sys
+import re
 
 
 def loadRandom(location, targetNum=1e5, seed=12345):
@@ -27,10 +28,12 @@ def loadRandom(location, targetNum=1e5, seed=12345):
     return pd.DataFrame(data)
 
 
-def loadRandom2(location, targetNum=1e5, seed=12345):
+def loadRandom2(location, targetNum=1e6, seed=12345):
     random.seed(seed)
     # number of records in file (excludes header)
-    n = sum(1 for line in open(location)) - 1
+    # n = sum(1 for line in open(location, encoding="utf8") if re.search(
+    #     "[0-9]{4}-[0-9]{2}-[0-9]{2} [0-9]{2}:[0-9]{2}:[0-9]{2}", line))
+    n = 6000000-1
     # the 0-indexed header will not be included in the skip list
     skip = sorted(random.sample(range(1, n+1), n-int(targetNum)))
-    return pd.read_csv(location, skiprows=skip)
+    return pd.read_csv(location, skiprows=skip, dtype={'text': str})
